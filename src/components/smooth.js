3caa59d717dd085c531aac2fdbedaa00a155b3c3
deduxer-studio <deduxer@gmail.com>
2022-12-20 09:16:54 -0800

    Update smooth.js

--- src/components/smooth.js
+++ src/components/smooth.js
@@ -19,8 +19,7 @@ export function initSmoothScroll() {
   ScrollSmoother.create({
     smooth: 1,
     wrapper: '[smooth-wrapper]',        // how long (in seconds) it takes to "catch up" to the native scroll position
-    effects: true,           // looks for data-speed and data-lag attributes on elements
-    smoothTouch: 0.1,        // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
+    effects: true,           // looks for data-speed and data-lag attributes on elements       // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
   });
 
   $('.navbar-nav a').each(function () {
