2a0f3717f9946a5db6dcaeb0e0cd7e5f8b715e80
deduxer-studio <deduxer@gmail.com>
2022-12-20 10:36:17 -0800

    Optimization v2

--- src/components/mesh.js
+++ src/components/mesh.js
@@ -114,56 +114,56 @@ export function initMesh() {
         console.log(deductionOptions.cam.zoom);
       },
     })
-    // let tl = gsap.timeline({
-    //   scrollTrigger: {
-    //     trigger: '.page-wrapper',
-    //     start: 'top top',
-    //     end: '+=' + document.querySelector('body').offsetHeight,
-    //     scrub: true,
-    //     onUpdate: (self) => {
-    //       currentZoom = deductionOptions.cam.zoom
-    //       deductionOptions.perlin.waves = Math.min(self.progress + 3 * 2, 15)
-    //     },
-    //   },
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.page-wrapper',
+        start: 'top top',
+        end: '+=' + document.querySelector('body').offsetHeight,
+        scrub: true,
+        onUpdate: (self) => {
+          currentZoom = deductionOptions.cam.zoom
+          deductionOptions.perlin.waves = Math.min(self.progress + 3 * 2, 15)
+        },
+      },
+    })
+    tl.fromTo(deductionOptions.cam, {
+      zoom: 20,
+    }, {
+      zoom: 10,
+    }, 'first')
+    tl.fromTo(deductionCamera.position, {
+      y: 0,
+      x: 0,
+    }, {
+      y: 2,
+      x: -3,
+    }, 'first')
+    // tl.fromTo(deductionCamera.position, {
+    //   y: 2,
+    //   x: 1,
+    // }, {
+    //   y: -2,
+    //   x: 0,
     // })
     // tl.fromTo(deductionOptions.cam, {
-    //   zoom: 20,
+    //   zoom: 2,
     // }, {
     //   zoom: 10,
-    // }, 'first')
-    // tl.fromTo(deductionCamera.position, {
-    //   y: 0,
-    //   x: 0,
+    // }, 'clients')
+    // tl.fromTo(deductionOptions.cam, {
+    //   zoom: 50,
     // }, {
-    //   y: 2,
-    //   x: -3,
-    // }, 'first')
-    // // tl.fromTo(deductionCamera.position, {
-    // //   y: 2,
-    // //   x: 1,
-    // // }, {
-    // //   y: -2,
-    // //   x: 0,
-    // // })
-    // // tl.fromTo(deductionOptions.cam, {
-    // //   zoom: 2,
-    // // }, {
-    // //   zoom: 10,
-    // // }, 'clients')
-    // // tl.fromTo(deductionOptions.cam, {
-    // //   zoom: 50,
-    // // }, {
-    // //   zoom: 3,
-    // // }, '+=1')
-    // tl.fromTo(deductionCamera.position, {
-    //   y: -2,
-    //   x: -3,
-    // }, {
-    //   y: 1,
-    //   x: 0,
-    // }, 'last')
+    //   zoom: 3,
+    // }, '+=1')
+    tl.fromTo(deductionCamera.position, {
+      y: -2,
+      x: -3,
+    }, {
+      y: 1,
+      x: 0,
+    }, 'last')
 
-    // return tl
+    return tl
 
 
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -20,137 +20,11 @@ export function initHome() {
     '[text-split], .heading-hero, .hero-content_block, .info__headings-itself, .work-title, .letter-wrap',
     {
       types: 'words, chars, lines',
-      tagName: 'divs',
+      tagName: 'span',
     }
   )
-  //desktop 
-  function Links() {
 
-    function getRandomLetter(length) {
-      var result = "";
-      var characters = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+{}|:<>?";
-      var charactersLength = characters.length;
-      for (var i = 0; i < length; i++) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-      }
-      return result;
-    }
-
-    $(".char").each(function (index) {
-      let text = $(this).text();
-      $(this).attr("letter", text);
-    });
-
-    $(".letter-wrap").each(function (index) {
-      function resetText() {
-        if (myInterval !== undefined) {
-          clearInterval(myInterval);
-        }
-        chars.each(function (index) {
-          let letter = $(this).attr("letter");
-          $(this).text(letter);
-        });
-      }
-
-      let myInterval;
-      let chars = $(this).find(".char");
-      $(this).on("mouseenter", function () {
-        let length = chars.length;
-        myInterval = setInterval(function () {
-          chars.each(function (index) {
-            if (index < length) {
-              let letter = getRandomLetter(1);
-              $(this).text(letter);
-            } else {
-              let letter = $(this).attr("letter");
-              $(this).text(letter);
-            }
-          });
-          length = length - 1;
-        }, 100);
-        setTimeout(() => {
-          resetText();
-        }, 600);
-      });
-      $(this).on("mouseleave", function () {
-        resetText();
-      });
-    });
-  }
-
-
-  function Indicator() {
-    let tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: '.main-wrapper',
-        start: 'top top',
-        endTrigger: '.main-wrapper',
-        end: '+=' + document.querySelector('.main-wrapper').offsetHeight,
-        scrub: true,
-        onUpdate: self => {
-          //check if element is in view
-          $('section').each(function (index) {
-
-            //check if this section is in view
-            if (
-              $(this).offset().top < $(window).scrollTop() + $(window).height() &&
-              $(this).offset().top + $(this).height() > $(window).scrollTop()
-            ) {
-
-              //add class to section
-              $(this).addClass('active');
-              $('.indicator-block').removeClass('active')
-              $('.indicator-block').eq(index).addClass('active')
-              //remove class from section
-              $(this).siblings().removeClass('active');
-            }
-          })
-
-
-          $(".marquee").each(function (index) {
-            let track = $(this).find(".marquee_track");
-            let items = $(this).find(".marquee_item");
-            let tl = gsap.timeline({ repeat: -1, defaults: { ease: "expo.inOut", duration: 1, delay: 1 } });
-
-            items.each(function (index) {
-              let distance = (index + 1) * -100;
-              tl.to(track, { yPercent: distance });
-            });
-
-            items.first().clone().appendTo(track);
-          });
-
-
-
-        }
-
-      }
-    })
-
-  }
-
-  function Scruber() {
-
-    $(".main_info").each(function () {
-      let tl = gsap.timeline({
-        scrollTrigger: {
-          trigger: $(this),
-          // trigger element - viewport
-          start: "top center",
-          end: "+=120%",
-          scrub: 1
-        }
-      });
-
-      tl.from($(this).find(".word"), {
-        opacity: 0.2,
-        stagger: { amount: 0.8 },
-      });
-    });
-
-  }
-
-  function Hero() {
+  function HeroMobile() {
     $(".heading-wrap").each(function (index) {
       let headings = $(this).find(".heading-hero");
       headings.each(function () {
@@ -166,54 +40,46 @@ export function initHome() {
       headings.each(function (index) {
 
         if (index > 0) {
-          tl.from($(this).find(".char"), { yPercent: 100, stagger: { amount: 0.2 }, duration: 1, ease: easeOut }, "<0.1");
+          tl.from($(this).find(".char"), { yPercent: 100, stagger: { amount: 0.25 }, duration: 1, ease: easeOut }, "<0.1");
         }
         if (index < headings.length - 1) {
-          tl.to($(this).find(".char"), { delay: 0.5, yPercent: -100, stagger: { amount: 0.2 }, duration: 0.8, ease: easeOut });
+          tl.to($(this).find(".char"), { delay: 0.3, yPercent: -100, stagger: { amount: 0.2 }, duration: 0.8, ease: easeOut });
         }
       });
     });
 
-    let tl = gsap.timeline()
-    tl.from('.hero-content-row', {
-      opacity: 0,
-      yPercent: 100,
-      stagger: { amount: 0.2 },
-      duration: 1,
-      //add class on timeline end
-      onComplete: function () {
-        const state = Flip.getState(".hero-content-row, .hero-content_block");
-        $(".hero-content-row").each(function (index) {
-          $(this).addClass("active");
-
-        });
-
-        Flip.from(state, {
-          absolute: true,
-          duration: 1,
-          stagger: 0.03,
-          ease: easeOut
-          // you can use any other tweening properties here too, like onComplete, onUpdate, delay, etc. 
-        });
-      },
-      ease: easeOut,
+    let tl = gsap.timeline({})
+    tl.delay(3.5);
+    tl.set('.hero-content_block .line', {
+      overflow: 'hidden',
     })
-      .from('.navbar', {
-        scaleY: 0,
-        ease: easeOut,
-        duration: 1,
-        transformOrigin: 'bottom',
-      })
-      .from('.navbar-logo', {
-        opacity: 0,
-      }, '<0.5')
-      .from('.navbar-navigation_link', {
-        opacity: 0,
-        yPercent: 100,
-        ease: easeOut,
-        duration: 1,
-        stagger: { amount: 0.2 },
-      }, '<0.2')
+    tl.from('.hero-content_block .char', {
+      yPercent: 100,
+      stagger: { amount: 0.8 },
+      ease: easeOut,
+      duration: 1.5,
+
+    })
+    return tl
+  }
+  function Clients() {
+    let swiper = new Swiper('.clients-slider', {
+      // Optional parameters
+      speed: 1500,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      }
+      // change slides speed
+    });
+
+    $('.swiper-button-next').on('click', function () {
+      swiper.slideNext()
+    })
+    $('.swiper-button-prev').on('click', function () {
+      swiper.slidePrev()
+    })
+
 
 
   }
@@ -360,131 +226,268 @@ export function initHome() {
 
   }
 
-  function Info() {
-    //scrolltrigger till end of section 
-    let tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: '.section_info',
-        start: 'top top',
-        end: '+=300%',
+  function Scruber() {
 
-        scrub: true,
+    $(".main_info").each(function () {
+      let tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: $(this),
+          // trigger element - viewport
+          start: "top center",
+          end: "+=120%",
+          scrub: 1
+        }
+      });
 
-      }
-    })
-
-
-
-  }
-
-  function Works() {
-    let tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: '.section_works',
-        start: 'top top',
-        scrub: true,
-        pin: true,
-        end: '+=40%',
-      }
-    })
-
-    tl.to('.background-works', {
-      width: '100%',
-      height: '100%',
-      borderRadius: 0,
-      ease: easeOut,
-    })
-
-
-
-  }
-
-  function Clients() {
-    let swiper = new Swiper('.clients-slider', {
-      // Optional parameters
-      speed: 1500,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      }
-      // change slides speed
+      tl.from($(this).find(".word"), {
+        opacity: 0.2,
+        stagger: { amount: 0.8 },
+      });
     });
 
-    $('.swiper-button-next').on('click', function () {
-      swiper.slideNext()
-    })
-    $('.swiper-button-prev').on('click', function () {
-      swiper.slidePrev()
-    })
-
-
-
   }
-  function Footer() {
-    let tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: '.component_footer',
-        start: 'top top',
-        pin: true,
-        scrub: true,
-        end: '+=80%',
+  function Links() {
+
+    function getRandomLetter(length) {
+      var result = "";
+      var characters = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+{}|:<>?";
+      var charactersLength = characters.length;
+      for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
       }
-    })
+      return result;
+    }
 
-    tl.from('.footer-bg', {
-      scaleX: 1.2,
-      borderRadius: 0,
-      ease: easeOut,
-    })
+    $(".char").each(function (index) {
+      let text = $(this).text();
+      $(this).attr("letter", text);
+    });
+
+    $(".letter-wrap").each(function (index) {
+      function resetText() {
+        if (myInterval !== undefined) {
+          clearInterval(myInterval);
+        }
+        chars.each(function (index) {
+          let letter = $(this).attr("letter");
+          $(this).text(letter);
+        });
+      }
+
+      let myInterval;
+      let chars = $(this).find(".char");
+      $(this).on("mouseenter", function () {
+        let length = chars.length;
+        myInterval = setInterval(function () {
+          chars.each(function (index) {
+            if (index < length) {
+              let letter = getRandomLetter(1);
+              $(this).text(letter);
+            } else {
+              let letter = $(this).attr("letter");
+              $(this).text(letter);
+            }
+          });
+          length = length - 1;
+        }, 100);
+        setTimeout(() => {
+          resetText();
+        }, 600);
+      });
+      $(this).on("mouseleave", function () {
+        resetText();
+      });
+    });
   }
+  //desktop 
+  if (window.innerWidth > 768) {
 
-  function HeroMobile() {
-    $(".heading-wrap").each(function (index) {
-      let headings = $(this).find(".heading-hero");
-      headings.each(function () {
-        gsap.set($(this), {
-          opacity: 1,
+    function Indicator() {
+      let tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: '.main-wrapper',
+          start: 'top top',
+          endTrigger: '.main-wrapper',
+          end: '+=' + document.querySelector('.main-wrapper').offsetHeight,
+          scrub: true,
+          onUpdate: self => {
+            //check if element is in view
+            $('section').each(function (index) {
+
+              //check if this section is in view
+              if (
+                $(this).offset().top < $(window).scrollTop() + $(window).height() &&
+                $(this).offset().top + $(this).height() > $(window).scrollTop()
+              ) {
+
+                //add class to section
+                $(this).addClass('active');
+                $('.indicator-block').removeClass('active')
+                $('.indicator-block').eq(index).addClass('active')
+                //remove class from section
+                $(this).siblings().removeClass('active');
+              }
+            })
+
+
+            $(".marquee").each(function (index) {
+              let track = $(this).find(".marquee_track");
+              let items = $(this).find(".marquee_item");
+              let tl = gsap.timeline({ repeat: -1, defaults: { ease: "expo.inOut", duration: 1, delay: 1 } });
+
+              items.each(function (index) {
+                let distance = (index + 1) * -100;
+                tl.to(track, { yPercent: distance });
+              });
+
+              items.first().clone().appendTo(track);
+            });
+
+
+
+          }
+
+        }
+      })
+
+    }
+
+
+
+    function Hero() {
+      $(".heading-wrap").each(function (index) {
+        let headings = $(this).find(".heading-hero");
+        headings.each(function () {
+          gsap.set($(this), {
+            opacity: 1,
+          });
+        });
+
+        let tl = gsap.timeline({ repeat: -1 });
+        tl.set($(this), { opacity: 1 });
+        tl.delay(4);
+
+        headings.each(function (index) {
+
+          if (index > 0) {
+            tl.from($(this).find(".char"), { yPercent: 100, stagger: { amount: 0.2 }, duration: 1, ease: easeOut }, "<0.1");
+          }
+          if (index < headings.length - 1) {
+            tl.to($(this).find(".char"), { delay: 0.5, yPercent: -100, stagger: { amount: 0.2 }, duration: 0.8, ease: easeOut });
+          }
         });
       });
 
-      let tl = gsap.timeline({ repeat: -1 });
-      tl.set($(this), { opacity: 1 });
-      tl.delay(4);
+      let tl = gsap.timeline()
+      tl.from('.hero-content-row', {
+        opacity: 0,
+        yPercent: 100,
+        stagger: { amount: 0.2 },
+        duration: 1,
+        //add class on timeline end
+        onComplete: function () {
+          const state = Flip.getState(".hero-content-row, .hero-content_block");
+          $(".hero-content-row").each(function (index) {
+            $(this).addClass("active");
 
-      headings.each(function (index) {
+          });
 
-        if (index > 0) {
-          tl.from($(this).find(".char"), { yPercent: 100, stagger: { amount: 0.25 }, duration: 1, ease: easeOut }, "<0.1");
+          Flip.from(state, {
+            absolute: true,
+            duration: 1,
+            stagger: 0.03,
+            ease: easeOut
+            // you can use any other tweening properties here too, like onComplete, onUpdate, delay, etc. 
+          });
+        },
+        ease: easeOut,
+      })
+        .from('.navbar', {
+          scaleY: 0,
+          ease: easeOut,
+          duration: 1,
+          transformOrigin: 'bottom',
+        })
+        .from('.navbar-logo', {
+          opacity: 0,
+        }, '<0.5')
+        .from('.navbar-navigation_link', {
+          opacity: 0,
+          yPercent: 100,
+          ease: easeOut,
+          duration: 1,
+          stagger: { amount: 0.2 },
+        }, '<0.2')
+
+
+    }
+
+
+    function Info() {
+      //scrolltrigger till end of section 
+      let tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: '.section_info',
+          start: 'top top',
+          end: '+=300%',
+
+          scrub: true,
+
         }
-        if (index < headings.length - 1) {
-          tl.to($(this).find(".char"), { delay: 0.3, yPercent: -100, stagger: { amount: 0.2 }, duration: 0.8, ease: easeOut });
+      })
+
+
+
+    }
+
+    function Works() {
+      let tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: '.section_works',
+          start: 'top top',
+          scrub: true,
+          pin: true,
+          end: '+=40%',
         }
-      });
-    });
+      })
 
-    let tl = gsap.timeline({})
-    tl.delay(3.5);
-    tl.set('.hero-content_block .line', {
-      overflow: 'hidden',
-    })
-    tl.from('.hero-content_block .char', {
-      yPercent: 100,
-      stagger: { amount: 0.8 },
-      ease: easeOut,
-      duration: 1.5,
+      tl.to('.background-works', {
+        width: '100%',
+        height: '100%',
+        borderRadius: 0,
+        ease: easeOut,
+      })
 
-    })
-    return tl
-  }
-  //document readdy
-  //if desktop
-  if (window.innerWidth > 1024) {
+
+
+    }
+
+
+    function Footer() {
+      let tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: '.component_footer',
+          start: 'top top',
+          pin: true,
+          scrub: true,
+          end: '+=80%',
+        }
+      })
+
+      tl.from('.footer-bg', {
+        scaleX: 1.2,
+        borderRadius: 0,
+        ease: easeOut,
+      })
+    }
+
+
     let master = gsap.timeline()
     master.add(Hero()).add(PinText()).add(Info()).add(Works()).add(Indicator()).add(Clients()).add(Footer()).add(Links()).add(Scruber())
 
   }
+  else {
 
-  if (window.innerWidth < 1024) {
     let master = gsap.timeline()
     master.add(HeroMobile()).add(Clients()).add(Links()).add(PinText()).add(Scruber())
 
@@ -497,5 +500,6 @@ export function initHome() {
 
 
 }
+
 
 
