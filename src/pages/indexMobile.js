dd4ad869de133f113a50c15677dca9b0ca394619
deduxer-studio <deduxer@gmail.com>
2022-12-23 18:59:52 -0800

    Update indexMobile.js

--- src/pages/indexMobile.js
+++ src/pages/indexMobile.js
@@ -223,7 +223,7 @@ export function initMobile() {
 
     }, 'same')
     tl.to('.navbar-menu_wrapper', {
-      height: '80vh',
+      height: '70vh',
       duration: 1.2,
       ease: easeOut,
       delay: .8
