1063dfd13d65857e1e02330fe2f1ea6f99cfe9f4
deduxer-studio <deduxer@gmail.com>
2022-12-28 06:24:37 -0800

    Vercel

--- vite.config.js
+++ vite.config.js
@@ -18,10 +18,15 @@ export default defineConfig({
     rollupOptions: {
       input: './src/main.js',
       output: {
+        format: 'umd',
         entryFileNames: 'main.js',
         esModule: false,
         compact: true,
-      }
+        globals: {
+          jquery: '$',
+        },
+      },
+      external: ['jquery'],
     },
   },
 })
